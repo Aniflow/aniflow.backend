9f8b682d74227e1835e847e4a28d39573f3ac14a
NebSec <vdlbsec@gmail.com>
2025-02-21 15:05:01 +0100

    Nicer imports

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from routers.anime import router as anime_router
-from routers.home import router as home_router
-from routers.user import router as user_router
+from routers import anime_router
+from routers import home_router
+from routers import user_router
 
 app = FastAPI(title="API Gateway")
 
---
+++ gateway/app/routers/__init__.py
@@ -0,0 +1,3 @@
+from .anime import router as anime_router  # Noqa: F401
+from .home import router as home_router  # Noqa: F401
+from .user import router as user_router  # Noqa: F401
